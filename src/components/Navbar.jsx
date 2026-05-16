9add14075d46792f1174ad18fd1f1be77aba29ee
Johann Balzereit <johannbalzereit99@gmail.com>
2026-05-16 10:26:51 +0200

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar () {
+  return (
+    <nav className="navbar">
+      <h1>E-Commerce Admin</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <ul>
+        <li>Dashboard</li>
+        <li>Products</li>
+        <li>Orders</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
