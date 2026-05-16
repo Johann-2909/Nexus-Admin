40eb06a938a7d42375689a46ffd59b862bf27ad5
Johann Balzereit <johannbalzereit99@gmail.com>
2026-05-16 10:43:21 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import Products from "./pages/Products";
+import Orders from "./pages/Orders";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Navbar />
+        <div className="flex">
+          <Sidebar />
+          <main className="flex-1">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/orders" element={<Orders />} />
+            </Routes>
+          </main>
+        </div>
+    </Router>
+  );
 }
 
 export default App;
